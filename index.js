469d38e72a8f26b8a0e69adcdc9cb3c294be2ebf
Syed Faiz Ahmed <sfahmed129@gmail.com>
2025-06-29 22:17:48 +0500

    First Tutorial (Half)

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express')
 const app = express()
 const port = 3000
@@ -10,6 +11,7 @@ app.get("/twitter", (req, res) => {
     res.send("faizdotcom")
 })
 
-app.listen(port, () => {
+
+app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
 })
